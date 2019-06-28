dc641c24e306dd144ea270c80b1e32ad9a0d0be0
Guilherme Failace <guilherme.failace@gmail.com>
2019-06-27 23:36:48 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Header from '../src/components/Header'
+import './styles.css'
+import Main from './pages/main'
+
+const App = () => (
+  <div className="App">
+    <Header />
+    <Main />
+  </div>
+)
+
+export default App;
