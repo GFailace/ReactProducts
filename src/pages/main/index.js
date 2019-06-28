4630650474932d5f6fa3ec5a8f76bf64b7bfc211
Guilherme Failace <guilherme.failace@gmail.com>
2019-06-28 09:39:11 -0300

    Add files via upload

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,5 +1,7 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { Component } from 'react';
 import api from '../../services/api'
+import './styles.css'
 
 export default class Main extends Component {
 
@@ -22,7 +24,11 @@ export default class Main extends Component {
         return (
             <div className="product-list">
                 {this.state.products.map(product => (
-                    <h2 key={product._id}>{product.title}</h2>
+                    <article className="product">
+                        <h2 key={product._id}>{product.title}</h2>
+                        <p>{product.description}</p>
+                        <a href=""><button>Acessar</button></a>
+                    </article>
                 ))}
             </div>
         )
